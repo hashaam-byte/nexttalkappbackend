65af2e2347f359bb72d99f20c6bb658e6fdabdae
hashaam <hashaamustafa@gmail.com>
2025-08-01 09:52:37 +0100

    sdff

--- routes/auth.js
+++ routes/auth.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
+const verifyToken = require('../middleware/verifyToken');
 
 // Auth endpoints
 router.post('/register', authController.register);
 router.post('/login', authController.login);
+router.get('/me', verifyToken, authController.me);
 
 module.exports = router;
-
-
 // Auth endpoints
 router.post('/register', authController.register);
 router.post('/login', authController.login);
